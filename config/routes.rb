386705e69af43ecbf77d3cebacb05e5ac399d2db
aljoh <reachingalfred@gmail.com>
2018-01-30 13:10:40 +0100

    request specs for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v0 do
       resources :ping, only: [:index], constrains: { format: 'json' }
     end
     namespace :v1, defaults: { format: :json } do
       resources :thoughts, only: [:create]
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
   end
 end
