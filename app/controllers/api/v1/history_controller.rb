f5d47ffcdffc7fc612e27341283014af2875858d
aljoh <reachingalfred@gmail.com>
2018-01-31 21:54:14 +0100

    request specs for history index action green

--- app/controllers/api/v1/history_controller.rb
+++ app/controllers/api/v1/history_controller.rb
@@ -1,3 +1,13 @@
 class Api::V1::HistoryController < ApplicationController
+  before_action :authenticate_api_v1_user!
 
+  def index
+    thoughts = []
+    current_user.thoughts.each do |thought|
+      if Time.strptime(thought.created_at.to_s, "%Y-%m-%d") == Time.strptime(params[:date], "%Y-%m-%d")
+        thoughts << thought
+      end
+    end
+    render json: thoughts, each_serializer: LabelsSerializer
+  end
 end
--- spec/requests/api/v1/history_spec.rb
+++ spec/requests/api/v1/history_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::HistoryController, type: :request do
+  let(:user) { FactoryBot.create(:user)}
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json'}.merge!(credentials) }
+  before do
+    3.times { FactoryBot.create(:thought, user: user) }
+    FactoryBot.create(:thought, user: user, created_at: Time.now.yesterday)
+  end
 
+  describe 'get /v1/history' do
+    it 'gets thoughts sorted by date' do
+      get '/api/v1/history?date=2018-01-31', headers: headers
+      expect(response.status).to eq 200
+      expected_response = eval(file_fixture('thoughts_for_date.txt').read)
+      expect(response_json).to eq expected_response.as_json
+    end
+  end
 end
