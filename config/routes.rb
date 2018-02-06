11f884d12ce32ce7a5826ec56d50026e6849df03
ScottB <17sb@cis.dk>
2018-02-06 10:34:48 +0100

    Remove extra route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'activity/index'
-    end
-  end
-
   mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v0 do
